9388fd671862beb2d2729e76609eaa49cd49f25a
Kelsey Kopecky <kkopecky@mines.edu>
2012-01-22 19:53:51 -0700

    Level 4 done

--- project02/rubywarrior/hamster-beginner/player.rb
+++ project02/rubywarrior/hamster-beginner/player.rb
@@ -1,18 +1,21 @@
 class Player
-  @health=warrior.health
+  def initialize
+    @health=20
+  end
   def play_turn(warrior)
     if warrior.feel.empty?
-	if warrior.health < 20
-		warrior.rest!
-	else
+	if @health > warrior.health or warrior.health == 20
 		warrior.walk!
+	else
+		warrior.rest!
 	end
     else
-	if warrior.health < 10
-		warrior.rest!
-	else
+	if warrior.health > 15 or @health > warrior.health
 		warrior.attack!
+	else
+		warrior.rest!
 	end
     end
+    @health=warrior.health
   end
 end
