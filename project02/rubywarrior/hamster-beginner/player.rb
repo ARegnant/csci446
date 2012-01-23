c273618f242dba815c0a806d8fd0a2aa0c79e4bf
Kelsey Kopecky <kkopecky@mines.edu>
2012-01-22 19:22:37 -0700

    First 3 levels done

--- project02/rubywarrior/hamster-beginner/player.rb
+++ project02/rubywarrior/hamster-beginner/player.rb
@@ -1,5 +1,18 @@
 class Player
+  @health=warrior.health
   def play_turn(warrior)
-    # add your code here
+    if warrior.feel.empty?
+	if warrior.health < 20
+		warrior.rest!
+	else
+		warrior.walk!
+	end
+    else
+	if warrior.health < 10
+		warrior.rest!
+	else
+		warrior.attack!
+	end
+    end
   end
 end
