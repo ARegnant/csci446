1f1295384b8cac1a4fbe9476021a6e11241276c1
Kelsey Kopecky <kkopecky@mines.edu>
2012-01-22 18:56:44 -0700

    Made project02 dir

---
+++ project02/ex1.rb
@@ -0,0 +1,13 @@
+line1 = "Hello World!"
+line2 = "Hello Again"
+line3 = "I like typing this."
+line4 = "This is fun."
+line5 = 'Yay! Printing.'
+line6 = "I'd much rather you 'not'."
+line7 = 'I "said" do not touch this.'
+line8 = "Another line lalala"
+
+print line5
+
+# Hashes are comments!
+
