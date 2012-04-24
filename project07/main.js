ed5fab63d71d3c8e23a41f03cd1c79541a5a8114
Kelsey Kopecky <kkopecky@mines.edu>
2012-04-23 20:40:20 -0600

    having trouble with cookies

--- project07/main.js
+++ project07/main.js
@@ -1,9 +1,8 @@
 var guessesLeft = 10;
-var highScores = new Array([9, "HarryJamesPotter"], [3, "ZedCthulhu"], [2, "NearlyDied"]);
 
 $(function() {
   updateScore(guessesLeft);
-  populateHighScores(highScores);
+  populateHighScores();
 });
 
 window.onload = function(){
@@ -14,11 +13,17 @@ function generateNumberToGuess(confirmIt) {
     numToGuess = Math.floor(Math.random()*100)+1;
 }
 
-function populateHighScores(scores) {
+function populateHighScores() {
+	var scrs = $.cookie('the_cookie');
+	cookiearray  = scrs.split(';');
+	for(var i=0; i < cookiearray.length; i++){
+		s = cookiearray[i].split('=')[0];
+		n = cookiearray[i].split('=')[1];
+	}
 	$('div#highScores').text("");
-  for (var i = 0; i < scores.length; ++i) {
-    $('div#highScores').append("<p>" + scores[i][0] + " " + scores[i][1] + "</p>");
-  }
+	for (var i = 0; i < s.length; ++i) {
+		$('div#highScores').append("<p>" + s[i][0] + " " + n[i][1] + "</p>");
+	}
 }
 
 function updateScore(score) {
@@ -30,12 +35,18 @@ function updateScore(score) {
 	}
 }
 
+
+
 function addScore(){
-	highScores.push([guessesLeft,document.getElementById("name").value]);
-	populateHighScores(highScores);
+	var scrs = $.cookie('the_cookie');
+	scrs = scrs + ";" + guessesLeft + "=" + document.getElementById("name").value;
+	$.cookie('the_cookie', scrs);
+	populateHighScores();
 	document.getElementById('newScore').style.display="none";
 	document.getElementById('output').style.display="none";
 }
+
+
 
 function yourGuess() {
     var guess = document.getElementById("guess").value;
@@ -84,3 +95,52 @@ function showNumberToGuess() {
 function newGame(){
 	generateNumberToGuess();
 }
+
+
+/*!
+ * jQuery Cookie Plugin
+ * https://github.com/carhartl/jquery-cookie
+ *
+ * Copyright 2011, Klaus Hartl
+ * Dual licensed under the MIT or GPL Version 2 licenses.
+ * http://www.opensource.org/licenses/mit-license.php
+ * http://www.opensource.org/licenses/GPL-2.0
+ */
+(function($) {
+    $.cookie = function(key, value, options) {
+
+        // key and at least value given, set cookie...
+        if (arguments.length > 1 && (!/Object/.test(Object.prototype.toString.call(value)) || value === null || value === undefined)) {
+            options = $.extend({}, options);
+
+            if (value === null || value === undefined) {
+                options.expires = -1;
+            }
+
+            if (typeof options.expires === 'number') {
+                var days = options.expires, t = options.expires = new Date();
+                t.setDate(t.getDate() + days);
+            }
+
+            value = String(value);
+
+            return (document.cookie = [
+                encodeURIComponent(key), '=', options.raw ? value : encodeURIComponent(value),
+                options.expires ? '; expires=' + options.expires.toUTCString() : '', // use expires attribute, max-age is not supported by IE
+                options.path    ? '; path=' + options.path : '',
+                options.domain  ? '; domain=' + options.domain : '',
+                options.secure  ? '; secure' : ''
+            ].join(''));
+        }
+
+        // key and possibly options given, get cookie...
+        options = value || {};
+        var decode = options.raw ? function(s) { return s; } : decodeURIComponent;
+
+        var pairs = document.cookie.split('; ');
+        for (var i = 0, pair; pair = pairs[i] && pairs[i].split('='); i++) {
+            if (decode(pair[0]) === key) return decode(pair[1] || ''); // IE saves cookies with empty string as "c; ", e.g. without "=" as opposed to EOMB, thus pair[1] may be undefined
+        }
+        return null;
+    };
+})(jQuery);
